e8bbecaf7abe8b0abd33ef1cd020fcf24a2a97a7
Richard D.G. Hickman <richard.hickman@hp.com>
2015-09-23 11:52:51 +0100

    Supporting new DecodeMethod in Codec

--- src/main/java/com/hpe/caf/codec/YamlCodec.java
+++ src/main/java/com/hpe/caf/codec/YamlCodec.java
@@ -3,8 +3,10 @@ package com.hpe.caf.codec;
 
 import com.hpe.caf.api.Codec;
 import com.hpe.caf.api.CodecException;
+import com.hpe.caf.api.DecodeMethod;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.error.YAMLException;
+import org.yaml.snakeyaml.representer.Representer;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -13,18 +15,32 @@ import java.nio.charset.StandardCharsets;
 
 /**
  * Use SnakeYAML to serialise/deserialise data.
+ *
+ * The strict version does not allow missing properties, whereas the lenient version does.
  */
 public class YamlCodec extends Codec
 {
-    private final Yaml yaml = new Yaml();
+    private final Yaml strictYaml;
+    private final Yaml lenientYaml;
+
+
+    public YamlCodec()
+    {
+        Representer lenient = new Representer();
+        lenient.getPropertyUtils().setSkipMissingProperties(true);
+        Representer strict = new Representer();
+        strict.getPropertyUtils().setSkipMissingProperties(false);
+        lenientYaml = new Yaml(lenient);
+        strictYaml = new Yaml(strict);
+    }
 
 
     @Override
-    public <T> T deserialise(final byte[] data, final Class<T> clazz)
+    public <T> T deserialise(final byte[] data, final Class<T> clazz, final DecodeMethod method)
             throws CodecException
     {
         try {
-            return yaml.loadAs(new ByteArrayInputStream(data), clazz);
+            return getYaml(method).loadAs(new ByteArrayInputStream(data), clazz);
         } catch (YAMLException e) {
             throw new CodecException("Failed to deserialise", e);
         }
@@ -32,11 +48,11 @@ public class YamlCodec extends Codec
 
 
     @Override
-    public <T> T deserialise(final InputStream stream, final Class<T> clazz)
+    public <T> T deserialise(final InputStream stream, final Class<T> clazz, final DecodeMethod method)
             throws CodecException
     {
         try {
-            return yaml.loadAs(stream, clazz);
+            return getYaml(method).loadAs(stream, clazz);
         } catch (YAMLException e) {
             throw new CodecException("Failed to deserialise", e);
         }
@@ -48,9 +64,15 @@ public class YamlCodec extends Codec
             throws CodecException
     {
         try {
-            return yaml.dump(object).getBytes(StandardCharsets.UTF_8);
+            return getYaml(DecodeMethod.getDefault()).dump(object).getBytes(StandardCharsets.UTF_8);
         } catch (YAMLException e) {
             throw new CodecException("Failed to serialise", e);
         }
+    }
+
+
+    private Yaml getYaml(final DecodeMethod method)
+    {
+        return method == DecodeMethod.STRICT ? strictYaml : lenientYaml;
     }
 }
--- src/test/java/com/hpe/caf/codec/YamlCodecTest.java
+++ src/test/java/com/hpe/caf/codec/YamlCodecTest.java
@@ -3,6 +3,7 @@ package com.hpe.caf.codec;
 
 import com.hpe.caf.api.Codec;
 import com.hpe.caf.api.CodecException;
+import com.hpe.caf.api.DecodeMethod;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,5 +23,31 @@ public class YamlCodecTest
         byte[] stuff = codec.serialise(test);
         CodecTestData res = codec.deserialise(stuff, CodecTestData.class);
         Assert.assertEquals(test.getTestString(), res.getTestString());
+    }
+
+
+    @Test(expected = CodecException.class)
+    public void testUnknownPropertyStrict()
+        throws CodecException
+    {
+        Codec codec = new YamlCodec();
+        ExtendedCodecTestData test = new ExtendedCodecTestData();
+        test.setTestString(VERIFY_STRING);
+        test.setTestValue(100);
+        byte[] stuff = codec.serialise(test);
+        codec.deserialise(stuff, CodecTestData.class, DecodeMethod.STRICT);
+    }
+
+
+    @Test
+    public void testUnknownPropertyLenient()
+        throws CodecException
+    {
+        Codec codec = new YamlCodec();
+        ExtendedCodecTestData test = new ExtendedCodecTestData();
+        test.setTestString(VERIFY_STRING);
+        test.setTestValue(100);
+        byte[] stuff = codec.serialise(test);
+        codec.deserialise(stuff, CodecTestData.class, DecodeMethod.LENIENT);
     }
 }
