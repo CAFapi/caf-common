685253c2c7c7b14a57ae1e4262537bd85636a4a0
Andrew Reid <andrew.reid@hpe.com>
2015-11-13 14:19:12 +0000

    1.1-SNASHOT

--- install-ca-cert.sh
+++ install-ca-cert.sh
@@ -8,8 +8,16 @@ then
         exit 1
     fi
 
-    cp -v $MESOS_SANDBOX/$SSL_CA_CRT /usr/local/share/ca-certificates/ssl-ca-cert.crt
-    update-ca-certificates
+    # Determine OS version
+    distribution=`awk -F= '/^ID=/{print $2}' /etc/os-release`
+    if [ "$distribution" == "debian" ]; then
+        cp -v $MESOS_SANDBOX/$SSL_CA_CRT /usr/local/share/ca-certificates/ssl-ca-cert.crt
+        update-ca-certificates
+    else
+        cp -v $MESOS_SANDBOX/$SSL_CA_CRT /etc/pki/ca-trust/source/anchors/ssl-ca-cert.crt
+        update-ca-trust
+    fi
+
 else
     echo "Not installing CA Certificate @ /usr/local/share/ca-certificates"
 fi
