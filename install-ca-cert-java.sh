912e501f44f682f97e7b2dd52b523c5a2d65c61d
Rob Duewer <rob.duewer@hpe.com>
2016-06-30 13:23:26 +0100

    Changes required to use public certs

--- install-ca-cert-java.sh
+++ install-ca-cert-java.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 
+storePass="changeit"
+caKeystorePath="/usr/lib/ssl/certs/java"
+
 if [ -n "$MESOS_SANDBOX" ] && [ -n "$SSL_CA_CRT" ]
 then
-    if ! [ -e $MESOS_SANDBOX/$SSL_CA_CRT ]
-    then
-        echo "CA Certificate at '$MESOS_SANDBOX/$SSL_CA_CRT' not found"
-        exit 1
-    fi
 
-    cd /usr/lib/ssl/certs/java
-    keytool -noprompt -keystore cacerts -storepass changeit -importcert -alias caf-ssl-ca-cert -file $MESOS_SANDBOX/$SSL_CA_CRT
+    cd $caKeystorePath
+    
+    IFS=',' read -a caFiles <<< "$SSL_CA_CRT"
+
+    for caFile in "${caFiles[@]}"
+    do
+        if ! [ -e $MESOS_SANDBOX/$caFile ]
+        then
+            echo "CA Certificate at '$MESOS_SANDBOX/$caFile' not found"
+            echo "Aborting further Java CA certificate load attempts."
+            exit 1
+        fi
+        
+        keytool -noprompt -keystore cacerts -storepass $storePass -importcert -alias $caFile -file $MESOS_SANDBOX/$caFile
+        echo "CA Certificate '$caFile' added to cacerts"
+    done
 
 else
-    echo "Not installing CA Certificate for Java"
+    echo "Not installing CA Certificate(s) for Java"
 fi
-
--- install-ca-cert.sh
+++ install-ca-cert.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
+#IMPORTANT: This version will only work with Debian based containers.
+
+caKeystorePath="/usr/local/share/ca-certificates"
+
 if [ -n "$MESOS_SANDBOX" ] && [ -n "$SSL_CA_CRT" ]
 then
-    if ! [ -e $MESOS_SANDBOX/$SSL_CA_CRT ]
-    then
-        echo "CA Certificate at '$MESOS_SANDBOX/$SSL_CA_CRT' not found"
-        exit 1
-    fi
 
-    cp -v $MESOS_SANDBOX/$SSL_CA_CRT /usr/local/share/ca-certificates/ssl-ca-cert.crt
-    update-ca-certificates
+    IFS=',' read -a caFiles <<< "$SSL_CA_CRT"
+
+    for caFile in "${caFiles[@]}"
+    do
+        if ! [ -e $MESOS_SANDBOX/$caFile ]
+        then
+            echo "CA Certificate at '$MESOS_SANDBOX/$caFile' not found"
+            echo "Aborting further system CA certificate load attempts."
+            exit 1
+        fi
+
+        cp -v $MESOS_SANDBOX/$caFile $caKeystorePath/$caFile.crt
+        update-ca-certificates
+
+        echo "CA Certificate '$caFile' added to system ca bundle"
+    done
+
 else
-    echo "Not installing CA Certificate @ /usr/local/share/ca-certificates"
+    echo "Not installing CA Certificate(s) to the system bundle"
 fi
