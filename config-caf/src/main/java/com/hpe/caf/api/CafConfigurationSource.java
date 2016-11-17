54856faba1f0c9156b3e79705a9ed1462365b29d
Dermot Hardy <dermot.hardy@hpe.com>
2016-11-17 22:38:05 +0000

    Whitespace changes to CafConfigurationSource

--- config-caf/src/main/java/com/hpe/caf/api/CafConfigurationSource.java
+++ config-caf/src/main/java/com/hpe/caf/api/CafConfigurationSource.java
@@ -1,6 +1,5 @@
 package com.hpe.caf.api;
 
-
 import com.hpe.caf.naming.Name;
 import com.hpe.caf.naming.ServicePath;
 import org.slf4j.Logger;
@@ -21,11 +20,10 @@ import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.atomic.AtomicInteger;
 
-
 /**
- * Partial implementation of a ManagedConfigurationSource that performs hierarchical lookups
- * based upon the service's ServicePath, and recursive lookup for configuration objects
- * that themselves have configuration in marked with the @Configuration annotation.
+ * Partial implementation of a ManagedConfigurationSource that performs hierarchical lookups based upon the service's ServicePath, and
+ * recursive lookup for configuration objects that themselves have configuration in marked with the @Configuration annotation.
+ *
  * @since 9.0
  */
 public abstract class CafConfigurationSource implements ManagedConfigurationSource
@@ -38,11 +36,10 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
     private final AtomicInteger confErrors = new AtomicInteger(0);
     private static final Logger LOG = LoggerFactory.getLogger(CafConfigurationSource.class);
 
-
     /**
-     * Each ConfigurationProvider itself takes some initial source of configuration
-     * which it may or may not use to initialise itself. The initial "bootstrap"
-     * configuration comes from the worker core itself.
+     * Each ConfigurationProvider itself takes some initial source of configuration which it may or may not use to initialise itself. The
+     * initial "bootstrap" configuration comes from the worker core itself.
+     *
      * @param bootstrapProvider the initial provider of configuration
      * @param cipher for decrypting information in a configuration file
      * @param servicePath to localise configuration for this service
@@ -57,14 +54,12 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
         Objects.requireNonNull(bootstrapProvider);
     }
 
-
     /**
-     * Acquire a configuration class from the provider. The requested class will be
-     * a simple Java object that when returned, and can be interacted with using getters
-     * and other standard mechanisms. Configuration classes may themselves contain other
-     * configuration objects, which will be recursively acquired if marked @Configuration.
-     * Any fields marked @Encrypted will be decrypted, any fields marked and any
-     * validation annotations will be processed.
+     * Acquire a configuration class from the provider. The requested class will be a simple Java object that when returned, and can be
+     * interacted with using getters and other standard mechanisms. Configuration classes may themselves contain other configuration
+     * objects, which will be recursively acquired if marked @Configuration. Any fields marked @Encrypted will be decrypted, any fields
+     * marked and any validation annotations will be processed.
+     *
      * @param configClass the class that represents your configuration
      * @param <T> the class that represents your configuration
      * @return the configuration class requested, if it can be deserialised
@@ -72,13 +67,13 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
      */
     @Override
     public final <T> T getConfiguration(final Class<T> configClass)
-            throws ConfigurationException
+        throws ConfigurationException
     {
         Objects.requireNonNull(configClass);
         incrementRequests();
         T config = getCompleteConfig(configClass);
-        Set<ConstraintViolation<T>> violations =  getValidator().validate(config);
-        if ( violations.isEmpty() ) {
+        Set<ConstraintViolation<T>> violations = getValidator().validate(config);
+        if (violations.isEmpty()) {
             return config;
         } else {
             incrementErrors();
@@ -86,7 +81,6 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
             throw new ConfigurationException("Configuration validation failed for " + configClass.getSimpleName());
         }
     }
-
 
     /**
      * {@inheritDoc}
@@ -97,7 +91,6 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
         return confRequests.get();
     }
 
-
     /**
      * {@inheritDoc}
      */
@@ -107,61 +100,56 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
         return confErrors.get();
     }
 
-
     protected Cipher getCipher()
     {
         return this.security;
     }
-
 
     protected ServicePath getServicePath()
     {
         return this.id;
     }
 
-
     protected Codec getCodec()
     {
         return this.codec;
     }
-
 
     protected Validator getValidator()
     {
         return this.validator;
     }
 
-
     /**
      * Acquire and return a stream of the serialised data from the transport source.
+     *
      * @param configClass the configuration class to be acquired
      * @param relativePath the partial service path that defines the scope to try and acquire the configuration in
      * @return the stream containing the serailised configuration of the class
      * @throws ConfigurationException if the stream cannot be acquired
      */
     protected abstract InputStream getConfigurationStream(final Class configClass, final Name relativePath)
-            throws ConfigurationException;
-
+        throws ConfigurationException;
 
     /**
-     * This is the recursive entry point for acquiring a complete configuration class
-     * to return. Attempt to acquire a deserialised object representing the configuration
-     * class requested, and analyse it for declared fields marked @Configuration. If any
-     * are found, the method recursively calls itself until all configuration is satisfied.
+     * This is the recursive entry point for acquiring a complete configuration class to return. Attempt to acquire a deserialised object
+     * representing the configuration class requested, and analyse it for declared fields marked @Configuration. If any are found, the
+     * method recursively calls itself until all configuration is satisfied.
+     *
      * @param configClass the class representing configuration to acquire
      * @param <T> the class representing configuration to acquire
      * @return the completed (at this level) configuration
      * @throws ConfigurationException if configuration cannot be acquired
      */
     private <T> T getCompleteConfig(final Class<T> configClass)
-            throws ConfigurationException
+        throws ConfigurationException
     {
         T config = getConfig(configClass);
-        for ( Field f : configClass.getDeclaredFields() ) {
-            if ( f.isAnnotationPresent(Configuration.class) ) {
+        for (Field f : configClass.getDeclaredFields()) {
+            if (f.isAnnotationPresent(Configuration.class)) {
                 try {
                     Method setter = new PropertyDescriptor(f.getName(), configClass).getWriteMethod();
-                    if ( setter != null ) {
+                    if (setter != null) {
                         setter.invoke(config, getCompleteConfig(f.getType()));
                     }
                 } catch (ConfigurationException e) {
@@ -170,11 +158,11 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
                     incrementErrors();
                     throw new ConfigurationException("Failed to get complete configuration for " + configClass.getSimpleName(), e);
                 }
-            } else if ( f.getType().equals(String.class) && f.isAnnotationPresent(Encrypted.class) ) {
+            } else if (f.getType().equals(String.class) && f.isAnnotationPresent(Encrypted.class)) {
                 try {
                     Method getter = new PropertyDescriptor(f.getName(), config.getClass()).getReadMethod();
                     Method setter = new PropertyDescriptor(f.getName(), config.getClass()).getWriteMethod();
-                    if ( getter != null && setter != null ) {
+                    if (getter != null && setter != null) {
                         setter.invoke(config, getCipher().decrypt((String) getter.invoke(config)));
                     }
                 } catch (CipherException | IntrospectionException | InvocationTargetException | IllegalAccessException e) {
@@ -185,22 +173,22 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
         return config;
     }
 
-
     /**
      * Acquire, deserialise and decrypt a configuration object from a data stream.
+     *
      * @param configClass the class representing configuration to acquire
      * @param <T> the class representing configuration to acquire
      * @return the deserialised configuration object
      * @throws ConfigurationException if the configuration cannot be acquired
      */
     private <T> T getConfig(final Class<T> configClass)
-            throws ConfigurationException
+        throws ConfigurationException
     {
         Iterator<Name> it = getServicePath().descendingPathIterator();
-        while ( it.hasNext() ) {
+        while (it.hasNext()) {
             try (InputStream in = getConfigurationStream(configClass, it.next())) {
                 return getCodec().deserialise(in, configClass);
-            } catch (ConfigurationException e ) {
+            } catch (ConfigurationException e) {
                 LOG.trace("No configuration at this path level", e);
             } catch (CodecException | IOException e) {
                 incrementErrors();
@@ -211,7 +199,6 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
         throw new ConfigurationException("No configuration found for " + configClass.getSimpleName());
     }
 
-
     /**
      * Increase the number of configuration requests recorded.
      */
@@ -219,7 +206,6 @@ public abstract class CafConfigurationSource implements ManagedConfigurationSour
     {
         this.confRequests.incrementAndGet();
     }
-
 
     /**
      * Increase the number of configuration errors recorded.
