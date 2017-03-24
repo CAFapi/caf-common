8c553b4873d884a55c595e4506d4143511c98580
smitcona <conal.smith@hpe.com>
2017-03-24 13:29:01 +0000

    Initial commit with a setup-tomcat-ssl-cert script. This script removes ssl connector commenting lines in an existing server.xml. It uses a provided keystore file. It only does this functionality if the SSL_TOMCAT_CA_CERT_LOCATION env variable is present - pointing to a keystore file.

---
+++ container-cert-script/setup-tomcat-ssl-cert.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+#
+# Copyright 2015-2017 Hewlett Packard Enterprise Development LP.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# If there is a provided tomcat CA certificate
+if [ -n "${SSL_TOMCAT_CA_CERT_LOCATION}" ]
+then
+    # search for and delete the lines in the server.xml with:
+    # <!-- setup-tomcat-ssl-cert.sh TLS section start
+    # setup-tomcat-ssl-cert.sh TLS section end -->
+    echo "Uncommenting SSL connector in /usr/local/tomcat/conf/server.xml"
+    sed -i '/setup-tomcat-ssl-cert.sh TLS section start/d' /usr/local/tomcat/conf/server.xml
+    sed -i '/setup-tomcat-ssl-cert.sh TLS section end/d' /usr/local/tomcat/conf/server.xml
+
+    # Replace the default keystore with the SSL_TOMCAT_CA_CERT_LOCATION keystore provided
+    echo "Replacing default keystore in /usr/local/tomcat/conf/server.xml with provided one."
+    sed -i "s@keystoreFile=.*@keystoreFile=\"$SSL_TOMCAT_CA_CERT_LOCATION\"@" /usr/local/tomcat/conf/server.xml
+else
+    echo "Not setting up tomcat SSL connector"
+fi
+
+# Replace default password with a user defined password if provided
+if [ -n "${SSL_TOMCAT_KEYSTORE_PASSWORD}" ]
+then
+    echo "Replacing password in /usr/local/tomcat/conf/server.xml with provided one."
+    sed -i "s@keystorePass=.*@keystorePass=\"$SSL_TOMCAT_KEYSTORE_PASSWORD\"@" /usr/local/tomcat/conf/server.xml
+fi
