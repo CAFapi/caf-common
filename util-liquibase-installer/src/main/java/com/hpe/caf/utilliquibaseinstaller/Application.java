cb5ffe9e2003814660117e02a8469bd33755222b
Anthony McGreevy <anthony.mcgreevy@microfocus.com>
2019-06-19 15:49:05 +0100

    SCMOD-6523: Added fix for Privacy Violation

--- util-liquibase-installer/src/main/java/com/hpe/caf/utilliquibaseinstaller/Application.java
+++ util-liquibase-installer/src/main/java/com/hpe/caf/utilliquibaseinstaller/Application.java
@@ -186,7 +186,7 @@ public class Application
                         liquibase = new Liquibase("changelog.xml", new ClassLoaderResourceAccessor(), database);
                     } else {
                         String errorMessage = "No liquibase changelog-master.xml or changelog.xml could be located";
-                        Logger.getLogger(Application.class.getName()).log(Level.SEVERE, errorMessage, this);
+                        Logger.getLogger(Application.class.getName()).log(Level.SEVERE, errorMessage);
                         throw new RuntimeException(errorMessage);
                     }
                     liquibase.getLog().setLogLevel(logLevel);
